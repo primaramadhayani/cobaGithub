beb17ec99d21bd6fe037e7d871faaf5d6d91533c
primaramadhayani <prima15ti@mahasiswa.pcr.ac.id>
2017-10-26 10:23:47 +0700

    J

---
+++ cobaGithub/pertama/coba_label.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class coba_label
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Label1.Text = "selamat datang"
+    End Sub
+
+End Class
